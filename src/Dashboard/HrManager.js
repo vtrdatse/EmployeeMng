215594c06bbfd1ab066387cf19511db35eac29c2
DK <datvotrongmod68@gmail.com>
2023-03-06 08:16:07 +0700

    [Datvt23]: Update Salary

--- src/Dashboard/HrManager.js
+++ src/Dashboard/HrManager.js
@@ -1,31 +1,40 @@
-import React from 'react'
-import Header from '../components/Header'
-import Banner from '../asset/pexels-pixabay-302769.jpg'
-import { USER } from '../DATA'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Header from "../components/Header";
+import Banner from "../asset/pexels-pixabay-302769.jpg";
+import { USER } from "../DATA";
+import { Link } from "react-router-dom";
 
 const HrManager = () => {
     return (
         <>
             <Header />
             <div className="container-fluid">
-
-                <div class="row mb-5" >
+                <div class="row mb-5">
                     <div class="col-sm-12 p-0">
-                        <div class="position-relative" style={{ height: '50vh' }}>
-                            <div class="bg-dark  position-absolute top-0 right-0 left-0 bottom-0 z-5 w-100 h-100" style={{
-                                opacity: 0.6
-                            }}></div>
+                        <div class="position-relative" style={{ height: "50vh" }}>
+                            <div
+                                class="bg-dark  position-absolute top-0 right-0 left-0 bottom-0 z-5 w-100 h-100"
+                                style={{
+                                    opacity: 0.6,
+                                }}
+                            ></div>
 
                             <img src={Banner} class="w-100 h-100" alt="Banner Image" />
-                            <div class="position-absolute w-100  d-flex flex-column justify-content-end top-50 start-50 translate-middle z-10" style={{
-                                top: '50%',
-                                transform: 'translateY(-50%)'
-                            }}>
+                            <div
+                                class="position-absolute w-100  d-flex flex-column justify-content-end top-50 start-50 translate-middle z-10"
+                                style={{
+                                    top: "50%",
+                                    transform: "translateY(-50%)",
+                                }}
+                            >
                                 <div class="container text-center text-white py-5 ">
                                     <h1 class="mb-4">Welcome to Dash board for HR</h1>
-                                    <p class="lead"></p>
-                                    <a href="#" class="btn btn-primary btn-lg mt-3">Learn More</a>
+                                    <p class="lead">
+
+                                    </p>
+                                    <a href="#" class="btn btn-primary btn-lg mt-3">
+                                        Learn More
+                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -33,28 +42,51 @@ const HrManager = () => {
                 </div>
 
                 <div className="row mt-5">
-                    <div className="col-sm-10 mx-auto" >
-                        <table className="table table-striped table-bordered">
+                    <div className="col-sm-10 mx-auto">
+                        <table className="table ">
                             <thead>
                                 <tr>
                                     <th>USERNAME</th>
                                     <th>NGÀY SINH </th>
-                                    <th width='200' className='text-center' >QUYỀN TRUY CẬP</th>
-                                    <th width='200' ></th>
-                                    <th width='200' ></th>
+                                    <th width="200" className="text-center">
+                                        QUYỀN TRUY CẬP
+                                    </th>
+                                    <th width="200"></th>
+                                    <th width="200"></th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {USER?.map(user =>
+                                {USER?.map((user) => (
                                     <tr>
                                         <td>{user.username}</td>
                                         <td>{user.birthday}</td>
-                                        <td className='text-center'>{user.role}</td>
-                                        <td className='text-center'><Link to={`/info/${user.id}`}>Xem thông tin</Link></td>
-                                        <td className='text-center'><Link to={`/salary/${user.id}`}>Xem bảng lương</Link></td>
-                                    </tr>)}
-
-
+                                        <td className="text-center">
+                                            <span
+                                                className={`badge ${user.role == "man"
+                                                    ? "badge badge-warning"
+                                                    : user.role == "hr"
+                                                        ? "badge badge-danger"
+                                                        : "badge-primary"
+                                                    } `}
+                                            >
+                                                {user.role}
+                                            </span>
+                                        </td>
+                                        <td className="text-center">
+                                            <Link className="btn btn-primary" to={`/info/${user.id}`}>
+                                                Xem thông tin
+                                            </Link>
+                                        </td>
+                                        <td className="text-center">
+                                            <Link
+                                                className="btn btn-warning"
+                                                to={`/salary/${user.id}`}
+                                            >
+                                                Xem bảng lương
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
@@ -63,12 +95,13 @@ const HrManager = () => {
                 <div className="row mt-5">
                     <div className="col-sm-12">
                         <div className="bg-dark text-white py-3 text-center">
-                            &copy; 2023 DSCT Building. All rights reserved.
+                            &copy; 2023 Vo Trong Dat. All rights reserved.
                         </div>
                     </div>
-                </div></div>
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default HrManager
+export default HrManager;
--- src/Dashboard/Salary.js
+++ src/Dashboard/Salary.js
@@ -1,42 +1,58 @@
-import React, { useEffect } from 'react'
-import Header from '../components/Header'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit } from '@fortawesome/free-regular-svg-icons';
-import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { useState } from 'react';
-import { SALARY, USER } from '../DATA';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import Header from "../components/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-regular-svg-icons";
+import {
+    faFacebookF,
+    faTwitter,
+    faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
+import { SALARY, USER } from "../DATA";
+import { useParams } from "react-router-dom";
 
 const Salary = () => {
-    const { id } = useParams()
-    const [currentUser, setCurrentUser] = useState(USER.find(USER => USER.id === id))
-    const [currentUserSalary, setCurrentUserSalary] = useState(null)
+    const { id } = useParams();
+    const [currentUser, setCurrentUser] = useState(
+        USER.find((USER) => USER.id === id)
+    );
+    const [currentUserSalary, setCurrentUserSalary] = useState(null);
     useEffect(() => {
-
-
         if (!currentUser) {
-            alert('not user')
-            return
+            alert("not user");
+            return;
         }
-        setCurrentUserSalary(SALARY.filter(salary => salary.ofEmployessId === id))
-    }, [id])
+        setCurrentUserSalary(SALARY.find((salary) => salary.ofEmployessId === id));
+    }, [id]);
 
-
+    //   console.log(currentUserSalary);
 
     return (
         <>
             <Header />
-            <div className='w-100 bg-dark position-fixed top-0' style={{ height: ' 60px' }}></div>
-            <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>
+            <div
+                className="w-100 bg-dark position-fixed top-0"
+                style={{ height: " 60px" }}
+            ></div>
+            <section className="vh-100" style={{ backgroundColor: "#f4f5f7" }}>
                 <div className="container-fluid py-5 h-100">
                     <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col col-lg-8 mb-4 mb-lg-0">
-                            <div className="card" style={{ borderRadius: '.5rem' }}>
+                        <div className="col col-lg-12 mb-4 mb-lg-0">
+                            <div className="card" style={{ borderRadius: ".5rem" }}>
                                 <div className="row g-0">
-                                    <div className="col-md-4 gradient-custom text-center text-white"
-                                        style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-                                            alt="Avatar" className="img-fluid my-5" style={{ width: '80px' }} />
+                                    <div
+                                        className="col-md-4 gradient-custom text-center text-white"
+                                        style={{
+                                            borderTopLeftRadius: ".5rem",
+                                            borderBottomLeftRadius: ".5rem",
+                                        }}
+                                    >
+                                        <img
+                                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                                            alt="Avatar"
+                                            className="img-fluid my-5"
+                                            style={{ width: "80px" }}
+                                        />
                                     </div>
                                     <div className="col-md-8">
                                         <div className="card-body p-4">
@@ -55,44 +71,59 @@ const Salary = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <table class="table table-striped table-hover table-bordered p-2">
-                                    <thead>
-                                        <tr>
-                                            <th >Mã</th>
-                                            <th >Tổng ngày làm việc</th>
-                                            <th >Tiền lương </th>
-                                            <th >Đơn vị</th>
-                                            <th >Ngày bắt đầu tính tiền</th>
-                                            <th >Đến ngày</th>
-                                            <th >Ngày tạo giao dịch</th>
-                                            <th >Ngày trả lương</th>
-                                            <th >Trạng thái</th>
-
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {currentUserSalary ? currentUserSalary?.map(user => <tr>
-                                            <td>{user.id}</td>
-                                            <td>{user.totalWorkingDays}</td>
-                                            <td>{user.amount}</td>
-                                            <td>{user.currencyUnit}</td>
-                                            <td>{user.fromDate}</td>
-                                            <td>{user.toDate}</td>
-                                            <td>{user.createdDate}</td>
-                                            <td>{user.paidDate}</td>
-                                            <td>{user.status}</td>
-                                        </tr>) : <p>Chưa có dữ liệu lương</p>}
-                                    </tbody>
-                                </table>
-
+                                {currentUserSalary ? (
+                                    <table class="table p-2 " style={{ overflow: "scroll" }}>
+                                        <thead>
+                                            <tr>
+                                                <th>Lương chính</th>
+                                                <th>Trách nhiệm</th>
+                                                <th>Ăn trưa</th>
+                                                <th>Điện thoại</th>
+                                                <th>Xăng xe</th>
+                                                <th>Hỗ trợ nhà ở</th>
+                                                <th>Tổng thu nhập</th>
+                                                <th>Ngày công thực tế</th>
+                                                <th>Tổng lương thực tế</th>
+                                                <th>Lương đóng BH</th>
+                                                <th>BHXH (8%)</th>
+                                                <th>BHYT (1.5%)</th>
+                                                <th>BHTN (1%)</th>
+                                                <th>Thuế TNCN</th>
+                                                <th>Tạm ứng</th>
+                                                <th>Thực lĩnh</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>{currentUserSalary.mainSalary}</td>
+                                                <td>{currentUserSalary.responsibility}</td>
+                                                <td>{currentUserSalary.lunch}</td>
+                                                <td>{currentUserSalary.phone}</td>
+                                                <td>{currentUserSalary.gas}</td>
+                                                <td>{currentUserSalary.stay}</td>
+                                                <td>{currentUserSalary.total}</td>
+                                                <td>{currentUserSalary.days}</td>
+                                                <td>{currentUserSalary.actualDays}</td>
+                                                <td>{currentUserSalary.insurance}</td>
+                                                <td>{currentUserSalary.BHYT}</td>
+                                                <td>{currentUserSalary.BHXH}</td>
+                                                <td>{currentUserSalary.BHTN}</td>
+                                                <td>{currentUserSalary.tax}</td>
+                                                <td>{currentUserSalary.temp}</td>
+                                                <td>{currentUserSalary.real}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                ) : (
+                                    <p class="text-center">Chưa có dữ liệu lương</p>
+                                )}
                             </div>
-
                         </div>
                     </div>
                 </div>
             </section>
         </>
-    )
-}
+    );
+};
 
-export default Salary
+export default Salary;
